5bcb761250cb172af335179a695ccced473caa66
bozo <ilbozo@gmail.com>
2022-01-08 14:45:13 +0100

    finally py3exiv2 on linux

--- main.py
+++ main.py
@@ -8,26 +8,48 @@ from PIL import Image
 
 
 
-# assign directory
-directory = 'E:\\Foto'
+def get_metadata_files():
+    # assign directory
+    directory = '/run/media/bozo/Sante/Foto'
+    # iterate over files in
+    # that directory
+    files = Path(directory).rglob('*')
+    xmp_files = []
+    exif_files = []
+    for filename in files:
+        if os.path.isfile(filename):
+            # list all the images
+            if filetype.is_image(filename):
+                # check if image has exif or xmp date
+                metadata = pyexiv2.ImageMetadata(str(filename))
+                metadata.read()
+                if len(metadata.xmp_keys)>0:
+                    xmp_files.append(filename)
+                if len(metadata.exif_keys):
+                    exif_files.append(filename)
 
-# iterate over files in
-# that directory
-files = Path(directory).rglob('*')
-for filename in files:
-    if os.path.isfile(filename):
-        # list all the images
-        if filetype.is_image(filename):
-            # check if image has exif or xmp date
-            metadata = pyexiv2.ImageData('test.jpg')
-            metadata.read()
-            if metadata.xmp_keys():
-                print(filename)
-            else:
-                print("not!!")
-        elif filetype.is_video(filename):
-            print(f"{filename} is a valid video...")
+            elif filetype.is_video(filename):
+                print(f"{filename} is a valid video...")
+    return xmp_files
 
+
+# xmp_files = get_metadata_files()
+# print("We're dealing with: {} files!")
+# for xmp_file in xmp_files:
+#     print(xmp_file)
+#     metadata = pyexiv2.ImageMetadata(str(xmp_file))
+#     metadata.read()
+#     print("-------", xmp_file, "-------")
+#     print(metadata.xmp_keys)
+#     break
+
+
+
+file = "/run/media/bozo/Sante/Foto/Backup/File Nikon100/175NIKON/DSCN1763.JPG"
+metadata = pyexiv2.ImageMetadata(str(file))
+metadata.read()
+print("-------", file, "-------")
+print(metadata.xmp_keys)
 
 
 # if it has xmp convert it to exif
